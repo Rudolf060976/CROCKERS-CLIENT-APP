4e559755e40123e418c11494d519073b248f092a
Rafael Urbina <rafaelurbinan@hotmail.com>
2020-05-03 15:33:39 -0400

    converting to a PWA

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 //import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-
-
 import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink, ApolloLink, concat } from '@apollo/client';
 
 import initialState from './local-state/initial-state';
@@ -23,6 +21,20 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Config from './config';
 
 import App from './App';
+
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/service-worker.js').then(registration => {
+			console.log('SW registered: ', registration);
+		
+		}).catch(registrationError => {
+			console.log('SW registration failed: ', registrationError);
+		});
+	});
+}
+
+
 
 const cache = new InMemoryCache();
  
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -8,6 +9,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -19,7 +21,39 @@ module.exports = {
      }),
 		new webpack.HotModuleReplacementPlugin(),
 		new BundleAnalyzerPlugin({ openAnalyzer: true }),
-		new MiniCssExtractPlugin()
+		new MiniCssExtractPlugin(),		
+	    new WorkboxPlugin.GenerateSW({
+	    // these options encourage the ServiceWorkers to get in there fast
+       // and not allow any straggling "old" SWs to hang around
+	    clientsClaim: true,
+	    skipWaiting: true,
+		}),
+		new WebpackPwaManifest({
+			name: "CROCKERS RESTAURANT WEBSITE",
+			short_name: "CROCKERS",
+			description: 'CROCKERS FOOD DELIVERY APP',
+			background_color: '#ffffff',
+			crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+			icons: [
+			  {
+				src: path.resolve('src/assets/images/ICON_192X192.png'),
+				sizes: [96, 128, 192] // multiple sizes
+			  },
+			  {
+				src: path.resolve('src/assets/images/ICON_512X512.png'),
+				sizes: [256, 384, 512] // multiple sizes
+			  }
+			  /*{
+				src: path.resolve('src/assets/large-icon.png'),
+				size: '1024x1024' // you can also use the specifications pattern
+			  },
+			  {
+				src: path.resolve('src/assets/maskable-icon.png'),
+				size: '1024x1024',
+				purpose: 'maskable'
+			  }*/
+			]
+		  })
      ],
   output: {
     filename: '[name].bundle.js',  //'main.js',
